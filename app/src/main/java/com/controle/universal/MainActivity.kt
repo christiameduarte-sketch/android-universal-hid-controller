9cb6ab988e172e26295bb5f742cd2d16b0336e46
christiameduarte-sketch <christiameduarte@gmail.com>
2026-07-11 16:14:00 -0300

    Update MainActivity.kt

--- app/src/main/java/com/controle/universal/MainActivity.kt
+++ app/src/main/java/com/controle/universal/MainActivity.kt
@@ -1,1 +1,18 @@
+package com.controle.universal
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "BluetoothController"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        Log.d(TAG, "Iniciando a lógica do controlador universal...")
+        // A lógica de Bluetooth será adicionada aqui em breve
+    }
+}

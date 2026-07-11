333c30dc9f61ced28d0fb06cdda092c1bd6a7807
christiameduarte-sketch <christiameduarte@gmail.com>
2026-07-11 16:39:48 -0300

    Update HidService.kt

--- app/src/main/java/com/controle/universal/app/src/main/app/src/main/java/com/controle/universal/app/src/main/java/com/controle/universal/HidService.kt
+++ app/src/main/java/com/controle/universal/app/src/main/app/src/main/java/com/controle/universal/app/src/main/java/com/controle/universal/HidService.kt
@@ -1,1 +1,23 @@
+package com.controle.universal
 
+import android.bluetooth.BluetoothHidDevice
+import android.bluetooth.BluetoothHidDeviceAppSdpSettings
+import android.bluetooth.BluetoothProfile
+import android.content.Context
+
+class HidService(private val context: Context, private val hidDevice: BluetoothHidDevice) {
+
+    fun registerApp() {
+        val sdpSettings = BluetoothHidDeviceAppSdpSettings(
+            "Universal Controller",
+            "Game Controller",
+            "Provider",
+            BluetoothHidDevice.SUBCLASS1_GAMEPAD,
+            HidDescriptor.GAMEPAD_REPORT_DESCRIPTOR
+        )
+
+        // Registrar o dispositivo como um Gamepad HID
+        // Nota: Em uma implementação completa, você passaria os callbacks aqui
+        hidDevice.registerApp(sdpSettings, null, null, null, { /* Ação de resposta */ })
+    }
+}
